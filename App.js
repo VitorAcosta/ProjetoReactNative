3ff9a41e61f3c30b309ec283ed696fb6554cd9c2
vi_acosta <55991392+VitorAcosta@users.noreply.github.com>
2020-05-07 17:16:39 -0300

    Versão 0705
    Parte visual montada, resta simulação.

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+import Routes from './src/routes';
+
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <Routes></Routes>
+    );
+  }
+}
